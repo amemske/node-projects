a499ddcc715a87c008278e94de70f7435b61644d
amemske <olywave@gmail.com>
2022-01-06 12:47:04 +0300

    handling errors

--- task_manager/middleware/error-handler.js
+++ task_manager/middleware/error-handler.js
@@ -1,7 +1,14 @@
 //custom midddleware error handler
-
+const { CustomAPIError } = require('../errors/custom-errors');
 const errorHandler = (err, req, res, next) => {
-  return res.status(500).json({ msg: err });
+  if (err instanceof CustomAPIError) {
+    //here the message and status are handled by inbuilt js constructor instead of moongose
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  console.log(err);
+  return res
+    .status(500)
+    .json({ msg: 'Something went wrong, please try again' });
 };
 
 module.exports = errorHandler;
